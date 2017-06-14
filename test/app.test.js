a5756636818f161c97f5135587f4bd8ac6222397
Akd's Git <achyutdeka@yahoo.com>
2017-06-14 22:27:32 +0530

    issue fixed

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,6 @@
 const expect = require('chai').expect
 describe('Test for runGenerator', function () {
   it('Test 1-', function () {
-    expect('page').to.eql('Page')
+    expect('page').to.eql('page')
   });
 });
