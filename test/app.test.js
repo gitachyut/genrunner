28c88fd727468860d90a0a1454010e980a3ccb3d
gitachyut <linkinakd72@gmail.com>
2017-06-14 15:53:36 +0530

    testing code

---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+// const runGenerator = require('../app')
+const expect = require('chai').expect
+describe('Test for runGenerator', function () {
+  it('Test 1-', function () {
+    expect('page').to.eql('page')
+  });
+});
