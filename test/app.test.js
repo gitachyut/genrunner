a1e2d793d8c297186882a95b35f3e4028c99e29f
Akd's Git <achyutdeka@yahoo.com>
2017-06-14 17:42:38 +0530

    Create app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,6 @@
 const expect = require('chai').expect
 describe('Test for runGenerator', function () {
   it('Test 1-', function () {
-    expect('page').to.eql('page')
+    expect('page').to.eql('Page')
   });
 });
